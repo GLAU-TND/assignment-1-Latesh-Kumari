ace2bf9f62bfcb8d38c174abd67fb0e2b7933ece
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-02-24 08:47:43 +0530

    create Person class

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -1,43 +1,77 @@
 package definition;
 
-import adt.ListADT;
+//import adt.ListADT;
 
-/**
- * This is the definition class of a contact list  (ADT). We will declare the body of the methods
- * defined in the ADT here. This class will implement (inherit) the ADT Interface defined
- * earlier.
- */
-public class Person<E> implements ListADT<E> {
-    private Node first;
-    private Node last;
 
-    public Person() {
-        first = last = null;
+import java.util.Objects;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String contactNumber;
+    private String email;
+
+    public Person(String firstName, String lastName, String contactNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactNumber = contactNumber;
+        this.email = email;
     }
 
-    public boolean isEmpty() {
-        return first == null;
+    public String getFirstName() {
+        return firstName;
     }
 
-    @Override
-    public boolean add(E item) {
-        Node temp = new Node();
-
-        return false;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    @Override
-    public E remove() {
-        return null;
+    public String getLastName() {
+        return lastName;
     }
 
-    @Override
-    public int search(E item) {
-        return 0;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    private class Node<E> {
+    public String getContactNumber() {
+        return contactNumber;
+    }
 
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "person{" +
+                "firstName='" + firstName + '\'' +
+                ",lastName='" + lastName + '\'' +
+                ",contactNumber=" + contactNumber +
+                ",email='" + email + '\'' +
+                '}';
+    }
+
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, contactNumber, email);
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(firstName, person.firstName) &&
+                Objects.equals(lastName, person.lastName) &&
+                Objects.equals(contactNumber, person.contactNumber) &&
+                Objects.equals(email, person.email);
 
     }
+
 }
